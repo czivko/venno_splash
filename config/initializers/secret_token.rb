1d09221c1618a095f90f3b53f989f1844679ae5b
ChaZiv <charleszivko@gmail.com>
2011-11-27 12:29:15 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VennoSplash::Application.config.secret_token = 'a89a682e8543da5f0138e5b1319436b08378cdb32903d83c469de094f4db343998a41ecd5a9317abbcdc4a74ad5e1592776421b6d5daf3043bf88636d989c78b'
